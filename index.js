d0370297e74c43fbfcda17af65976ad7c73f10b4
marufbroh <marufmd775@gmail.com>
2023-05-02 17:35:46 +0600

    ten chef data added on json file

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const chefData = require('./chef_data.json')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chef-data', (req, res) => {
+    res.send(chefData)
 })
 
 app.listen(port, () => {
