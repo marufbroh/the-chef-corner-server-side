43a7cccc5c7908a9ac84aa2d30921c07dccbbcfa
marufbroh <marufmd775@gmail.com>
2023-05-03 14:26:13 +0600

    cros updated

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000
+app.use(cors());
+
 
 const chefData = require('./chef_data.json')
 
